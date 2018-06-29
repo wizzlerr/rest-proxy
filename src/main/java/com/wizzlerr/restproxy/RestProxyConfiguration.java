e5277b95ce75bebf4a79741b8439c2d790cfba6b
Adam Sabel <sabel.adam@gmail.com>
2018-06-29 23:57:36 +0200

    Repackaging for simple proxy

--- src/main/java/com/wizzlerr/restproxy/RestProxyConfiguration.java
+++ src/main/java/com/wizzlerr/restproxy/RestProxyConfiguration.java
@@ -1,4 +1,4 @@
-package com.wizzlerr.restproxy.configuration;
+package com.wizzlerr.restproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
--- src/main/java/com/wizzlerr/restproxy/router/ExternalRoutingConfiguration.java
+++ src/main/java/com/wizzlerr/restproxy/router/ExternalRoutingConfiguration.java
@@ -1,6 +1,5 @@
-package com.wizzlerr.restproxy.configuration;
+package com.wizzlerr.restproxy.router;
 
-import com.wizzlerr.restproxy.router.ExternalRouter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -10,10 +9,10 @@ import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
-public class RoutingConfiguration {
+public class ExternalRoutingConfiguration {
 
     @Bean
     public RouterFunction<ServerResponse> monoExternalRouter(final ExternalRouter externalRouter) {
-        return RouterFunctions.route(RequestPredicates.GET("/api").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), externalRouter::route);
+        return RouterFunctions.route(RequestPredicates.GET("/api/*").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), externalRouter::route);
     }
 }
