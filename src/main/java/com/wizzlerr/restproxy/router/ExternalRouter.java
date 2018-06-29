dd88939d922b1716ffb393a419d5d1b39ac82159
Adam Sabel <sabel.adam@gmail.com>
2018-06-29 22:55:23 +0200

    Enabling actuators

--- src/main/java/com/wizzlerr/restproxy/router/ExternalRouter.java
+++ src/main/java/com/wizzlerr/restproxy/router/ExternalRouter.java
@@ -1,16 +1,19 @@
 package com.wizzlerr.restproxy.router;
 
 import com.wizzlerr.restproxy.stereotype.Router;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Router
 public class ExternalRouter {
 
     public Mono<ServerResponse> route(final ServerRequest serverRequest) {
+        log.info("Routing request {}", serverRequest);
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromObject("Hello World!"));
     }
 }
