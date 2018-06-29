826f30c7f2cbbb627aa3edec7c5d58b0756bb58d
Adam Sabel <sabel.adam@gmail.com>
2018-06-29 18:58:11 +0200

    Plain config, adding slf4j

---
+++ src/main/java/com/wizzlerr/restproxy/RestProxyConfiguration.java
@@ -0,0 +1,12 @@
+package com.wizzlerr.restproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProxyConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestProxyConfiguration.class, args);
+    }
+}
